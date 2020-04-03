13e5a6ca8dddf5d0d5e9c362f828209c92419465
Tijs <tijs.martens@student.hogent.be>
2020-04-03 21:06:25 +0200

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import Geolocation from './components/Geolocation'
+
+import "./App.css";
 
 function App() {
+  const routes = (
+    <Switch>
+      <Route path="/" component={Geolocation} />
+    </Switch>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Learn React</header>
     </div>
   );
 }
---
+++ src/components/Geolocation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Geolocation = props => {
+
+  return(
+    <div>
+      ejjooo
+    </div>
+  )
+}
+
+
+export default Geolocation
