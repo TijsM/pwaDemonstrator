bd16eb90503b5345d84936c017470bb2bd1300e5
Tijs <tijs.martens@student.hogent.be>
2020-04-03 22:36:24 +0200

    added scraped location

--- src/components/Geolocation.js
+++ src/components/Geolocation.js
@@ -5,13 +5,24 @@ const Geolocation = () => {
     status: "noPermission",
   });
 
+  const [address, setAddress] = useState(null)
+
   useEffect(() => {
-    const found = (location) => {
+    const found = async (location) => {
       setLocation({
         status: "found",
         data: location,
       });
+
+
+      let _address = await fetch('https://api.opencagedata.com/geocode/v1/json?q=50.96137%2C%203.60648&key=e1f1763b686147ae9ed70bb29fab87e3&language=en&pretty=1')
+      _address = await _address.json()
+
+      console.log('address', _address)
+
+      setAddress(_address.results[0].formatted)
     };
+
 
     const error = (error) => {
       setLocation({
@@ -23,17 +34,21 @@ const Geolocation = () => {
     navigator.geolocation.getCurrentPosition(found, error);
   }, []);
 
-  console.log(location);
-  const locationJsx = location.status === "found" && <div>
-    longitude = {location.data.coords.longitude}
-    latitude = {location.data.coords.latitude}
-  </div>;
+  const locationJsx = location.status === "found" && (
+    <div>
+      <div> longitude = {location.data.coords.longitude}</div>
+      <div>latitude = {location.data.coords.latitude}</div>
+    </div>
+  );
 
+  console.log('adres', address)
   return (
     <div>
       {location.status === "noPermission" && "asking for permissions"}
       {location.status === "error" && "error"}
       {location.status === "found" && locationJsx}
+
+      {address? <div>oh yeah, and I know where you are:  {address}</div>: null}
     </div>
   );
 };
