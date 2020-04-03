63e3fcfbb63675efa2c55a8a210f97686d39ffea
Tijs <tijs.martens@student.hogent.be>
2020-04-03 22:19:03 +0200

    get location

--- src/components/Geolocation.js
+++ src/components/Geolocation.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
+const Geolocation = () => {
+  const [location, setLocation] = useState({
+    status: "noPermission",
+  });
 
-const Geolocation = props => {
+  useEffect(() => {
+    const found = (location) => {
+      setLocation({
+        status: "found",
+        data: location,
+      });
+    };
 
-  return(
+    const error = (error) => {
+      setLocation({
+        status: "error",
+        data: error,
+      });
+    };
+
+    navigator.geolocation.getCurrentPosition(found, error);
+  }, []);
+
+  console.log(location);
+  const locationJsx = location.status === "found" && <div>
+    longitude = {location.data.coords.longitude}
+    latitude = {location.data.coords.latitude}
+  </div>;
+
+  return (
     <div>
-      ejjooo
+      {location.status === "noPermission" && "asking for permissions"}
+      {location.status === "error" && "error"}
+      {location.status === "found" && locationJsx}
     </div>
-  )
-}
+  );
+};
 
-
-export default Geolocation
+export default Geolocation;
