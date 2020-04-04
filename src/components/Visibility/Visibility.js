d6b08d107801ccab1e707f4d526df47263ee3fcd
Tijs <tijs.martens@student.hogent.be>
2020-04-04 11:25:28 +0200

    added hidden + logging

--- src/components/Visibility/Visibility.js
+++ src/components/Visibility/Visibility.js
@@ -2,28 +2,51 @@ import React, { useState } from "react";
 
 const Visibility = () => {
   const [focus, setFocus] = useState(document.hasFocus());
+  const [hidden, setHidden] = useState(document.hidden);
 
   setInterval(() => {
     setFocus(document.hasFocus());
-  }, 300);
+    setHidden(document.hidden);
+  }, 1000);
+
+  hidden
+    ? console.log("%cthe page was hidden", "color: red")
+    : console.log("%cthe page is vissible", "color: green");
 
   return (
     <div>
       <h1>Focus and Visibility</h1>
       <div className="headerCaption">
         focus detects if the user is using the webpage <br />
-        Visibility detects if the app is in use (minimized or not)
+        Visibility detects if the app is in use (minimized or not) (so you will
+        probably never see the ✅ here 😉), but take a look in the log files
+        after minimizing.
       </div>
-      focus:
-      {focus ? (
-        <span role="img" aria-label="check">
-          ✅
-        </span>
-      ) : (
-        <span role="img" aria-label="cross">
-          ❌
-        </span>
-      )}
+      <div>
+        focus:
+        {focus ? (
+          <span role="img" aria-label="check">
+            ✅
+          </span>
+        ) : (
+          <span role="img" aria-label="cross">
+            ❌
+          </span>
+        )}
+      </div>
+
+      <div>
+        hidden:
+        {hidden ? (
+          <span role="img" aria-label="check">
+            ✅
+          </span>
+        ) : (
+          <span role="img" aria-label="cross">
+            ❌
+          </span>
+        )}
+      </div>
     </div>
   );
 };
