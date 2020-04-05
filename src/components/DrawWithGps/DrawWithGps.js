b6b436c408a61b0c202fab48f06bc274cbdd5161
Tijs <tijs.martens@student.hogent.be>
2020-04-05 11:27:34 +0200

    added difference logic

--- src/components/DrawWithGps/DrawWithGps.js
+++ src/components/DrawWithGps/DrawWithGps.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from "react";
 
 const DrawWithGps = () => {
-
   const [locations, setLocations] = useState([]);
 
   useEffect(() => {
@@ -10,7 +9,7 @@ const DrawWithGps = () => {
       _state.push({
         accuracy: pos.coords.accuracy,
         latitude: pos.coords.latitude,
-        longitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
       });
 
       setLocations(_state);
@@ -28,28 +27,35 @@ const DrawWithGps = () => {
     navigator.geolocation.watchPosition(success, error, options);
   }, [locations, setLocations]);
 
-  console.log(locations);
+  
+  const calcRelativeMovement = (long1, lat1, long2, lat2) => {
+    const verticalMovement = long1 - long2;
+    const horizontalMovement = lat1 - lat2; 
 
-  const locationsJSX = locations.map((loc) => {
-    return (
-      <div key={Math.random()} className="location">
-        <div>accuracy: {loc.accuracy}</div>
-        <div>longitude: {loc.longitude}</div>
-        <div>latitude: {loc.latitude}</div>
-      </div>
-    );
-  });
+    const relativeVerticalMovement = verticalMovement/90;
+    const relatiHorizontalMovement = horizontalMovement/90;
+    
+
+    return({
+      vertical: relativeVerticalMovement,
+      horizontalMovement: relatiHorizontalMovement
+    })
+  }
+
+  console.log(calcRelativeMovement(3.6064212, 50.9613447, 3.6064213,50.9613447))
+  console.log(locations);
 
   return (
     <div>
       <h1>Draw by moving</h1>
       <div className="headerCaption">
-       the path where you are walking will be a painting! Be creative and create something awesome!
+        the path where you are walking will be a painting! Be creative and
+        create something awesome!
+        <br /> I created a canvas of 200meters by 200 meter for you! Go ahead
+        and create some art.
       </div>
-      {locationsJSX}
     </div>
   );
-}
+};
 
-
-export default DrawWithGps
+export default DrawWithGps;
