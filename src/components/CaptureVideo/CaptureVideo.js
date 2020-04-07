439128a99ccf20184587b48495481d6b933a382e
Tijs <tijs.martens@student.hogent.be>
2020-04-07 08:46:27 +0200

    get video stream to output on screen

--- src/components/CaptureVideo/CaptureVideo.js
+++ src/components/CaptureVideo/CaptureVideo.js
@@ -1,10 +1,37 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
 
 const CaptureVideo = () => {
+  const [vidSource, setVidSource] = useState(null);
 
-  return(<div>captureVideo</div>)
-}
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        setVidSource(stream);
+        const vid = document.getElementById("vid");
+        vid.srcObject = stream;
+      })
+      .catch(console.error);
+  }, []);
 
+  console.log(vidSource);
+  if (vidSource) {
+    console.log(vidSource.getVideoTracks()[0]);
+  }
 
-export default CaptureVideo
+  return (
+    <div>
+      <h1>Grab a video</h1>
+      <video
+        id="vid"
+        width="500"
+        height="500"
+        src={vidSource}
+        autoPlay
+        controls
+      ></video>
+    </div>
+  );
+};
+
+export default CaptureVideo;
