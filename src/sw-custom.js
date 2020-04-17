5f3725df26f34c69281e012523c2e5c1aab77920
Tijs <tijs.martens@student.hogent.be>
2020-04-17 08:41:08 +0200

    cleaned sw

--- src/sw-custom.js
+++ src/sw-custom.js
@@ -1,72 +1,21 @@
-if ("function" === typeof importScripts) {
-  importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js"
-  );
+/* eslint-disable*/
 
-  console.log('here')
-  // Global workbox
-  if (workbox) {
-    console.log("Workbox is loaded");
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js"
+);
 
-    // Disable logging
-    workbox.setConfig({ debug: false });
 
-    //`generateSW` and `generateSWString` provide the option
-    // to force update an exiting service worker.
-    // Since we're using `injectManifest` to build SW,
-    // manually overriding the skipWaiting();
-    self.addEventListener("install", (event) => {
-      self.skipWaiting();
-      window.location.reload();
-    });
+self.addEventListener("install", (event) => {
+  console.log("[Service Worker] installing Service Worker ....", event);
+  self.skipWaiting();
+});
 
-    // Manual injection point for manifest files.
-    // All assets under build/ and 5MB sizes are precached.
-    workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+self.addEventListener("activate", function (event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
+});
 
 
 
-    // Font caching
-    workbox.routing.registerRoute(
-      new RegExp("https://fonts.(?:.googlepis|gstatic).com/(.*)"),
-      workbox.strategies.cacheFirst({
-        cacheName: "googleapis",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 30,
-          }),
-        ],
-      })
-    );
 
-    // Image caching
-    workbox.routing.registerRoute(
-      /\.(?:png|gif|jpg|jpeg|svg)$/,
-      workbox.strategies.cacheFirst({
-        cacheName: "images",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 60,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-          }),
-        ],
-      })
-    );
 
-    // JS, CSS caching
-    workbox.routing.registerRoute(
-      /\.(?:js|css)$/,
-      workbox.strategies.staleWhileRevalidate({
-        cacheName: "static-resources",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 60,
-            maxAgeSeconds: 20 * 24 * 60 * 60, // 20 Days
-          }),
-        ],
-      })
-    );
-  } else {
-    console.error("Workbox could not be loaded. No offline support");
-  }
-}
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
